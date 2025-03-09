6a6fba530b0a2205d81b49f6f26868e498d9fadd
Jaskaran Singh <ok.jaskaran@gmail.com>
2025-03-10 01:04:33 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Enable static export for GitHub Pages
-  output: "export",
-
-  // Optional: You can add additional settings if needed
-  reactStrictMode: true, // Enables React strict mode
-  trailingSlash: true, // Ensures that URLs have a trailing slash, e.g., `/about/` instead of `/about`
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
