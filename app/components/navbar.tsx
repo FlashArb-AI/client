b842d0f7812b44373523e6e5ab2baf6fdc82308b
Jaskaran Singh <ok.jaskaran@gmail.com>
2025-03-09 15:21:07 +0530

    latest

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,37 +2,39 @@
 
 import logo from "@/public/logo.png";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <div className="px-12 h-16 flex items-center justify-between font-semibold fixed top-0 left-0">
+    <div className="px-12 h-16 flex items-center justify-between font-semibold fixed top-0 left-0 w-full">
       <div className="flex flex-row gap-12">
         <div className="flex flex-row justify-center items-center gap-3 font-bold">
-          {" "}
           <Image src={logo} alt="logo" width={35} height={35} />
           FlashArb-AI
         </div>
-
-        <a
+        <Link
           href="/"
-          className={` hover:text-gray-300 ${
-            window.location.pathname === "/"
-              ? "border-b-2 border-[var(--foreground)]"
-              : ""
+          className={`hover:text-gray-300 ${
+            pathname === "/" ? "border-b-2 border-[var(--foreground)]" : ""
           }`}
         >
           Agent
-        </a>
-        <a
+        </Link>
+        <Link
           href="/dashboard"
-          className={` hover:text-gray-300 ${
-            window.location.pathname === "/dashboard"
+          className={`hover:text-gray-300 ${
+            pathname === "/dashboard"
               ? "border-b-2 border-[var(--foreground)]"
               : ""
           }`}
         >
           Dashboard
-        </a>
+        </Link>
       </div>
+      <w3m-button />
     </div>
   );
 }
